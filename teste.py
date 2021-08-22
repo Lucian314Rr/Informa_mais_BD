f714492ca6daa9f621101de071d15a49a2f9ea8e
Lucian3,14r <78571686+Lucian314Rr@users.noreply.github.com>
2021-08-22 17:04:41 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -14,10 +14,16 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 #Aqui é onde a mágica acontece. Através dessa linha executando esse método, o que
-#estiver entre as aspas duplas será executado no Banco propriamente dito.
+#estiver entre as aspas duplas será executado no Banco propriamente dito
 #Resumindo, é a ÚNICA parte do código que iramos mexer, por enquanto
-mycursor.execute("Código MySQL aqui")
-
+mycursor.execute("CRAETE DATABASE IMFORMA_MAIS")
+mycursor.execute("CRAETE TABLE Cidadao(nome VARCHAR(30) NOT NULL, PRIMARY KEY email VARCHAR(30) NOT NULL, data DATE NOT NULL, telefone VARCHAR(12) NOT NULL, FOREIGN KEY ID_Relato INTERGER NOT NULL)")
+mycursor.execute("CRAETE TABLE Endereco(rua VARCHAR(25) NOT NULL, bairro VARCHAR(20) NOT NULL, cidade VARCHAR(15) NOT NULL, cep VARCHAR(10) NOT NULL, numero VARCHAR(4) NOT NULL, FOREIGN KEY email_cidadao VARCHAR(35) NOT NULL,)")
+mycursor.execute("CRAETE TABLE Relato(descricao VARCHAR(100) NOT NULL, PRIMARY KEY ID_relato INTEGER NOT NULL, data DATETIME NOT NULL, FOREIGN KEY status BOoLEAN NOT NULL)")#nao sei como colocar categoria_problema, subcategoria_problema, localizacao, midia
+mycursor.execute("CRAETE TABLE Prestador de servico publico(nome VARCHAR(30) NOT NULL, orgao/empresa VARCHAR(15) NOT NULL, PRIMARY KEY ID_funcionario INTERGER)")
+mycursor.execute("CRAETE TABLE Midia(foto INTEGER NOT NULL, video NOT NULL, FOREIGN KEY nome VARCHAR(30) NOT NULL)")
+mycursor.execute("CRAETE TABLE Problema(PRIMERY KEY categoria VARCHAR(20) NOT NULL, PRIMARY KEY subcategoria VARCHAR(20) NOT NULL, FOREIGN KEY ID_relato INTEGER NOT NULL)")
+mycursor.execute("CRAETE TABLE Localizacao(rua VARCHAR(15) NOT NULL, bairro VARCHAR(20) NOT NULL, cidade VARCHAR(20) NOT NULL, cep VARCHAR(10) NOT NULL, FOREIGN KEY ID_Relato INTERGER NOT NULL)")
 #Sinceramente não me lembro pra quê serve isso, mas sei que é necessário
 #Quem souber, deixa aqui nos comentários
 myresult = mycursor.fetchall()
